3f12a67ec4ed01f8f26f72346359b012284e8377
Mikhail Turlyakov <reelkek@gmail.com>
2023-10-09 17:22:37 +0400

    add circle shape; add triangle shape

---
+++ ShapesLibrary/Circle.cs
@@ -0,0 +1,16 @@
+﻿namespace ShapesLibrary;
+
+public class Circle : IShape
+{
+    private readonly double _radius;
+
+    public Circle(double radius)
+    {
+        _radius = radius; 
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
---
+++ ShapesLibrary/Triangle.cs
@@ -0,0 +1,40 @@
+﻿namespace ShapesLibrary;
+
+public class Triangle : IShape
+{
+    private const int QuadraticFactor = 2;
+    private double _sideA;
+    private double _sideB;
+    private double _sideC;
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        _sideA = sideA;
+        _sideB = sideB;
+        _sideC = sideC;
+    }
+
+    /// <summary>
+    /// Вычисление площади треугольника по формуле Герона
+    /// </summary>
+    public double GetArea()
+    {
+        double p = (_sideA + _sideB + _sideC) / QuadraticFactor;
+        return Math.Sqrt(p * (p - _sideA) * (p - _sideB) * (p - _sideC));
+    }
+
+    /// <summary>
+    /// Проверка прямоугольности по теореме Пифагора 
+    /// </summary>
+    public bool IsRightAngled()
+    {
+        double eps = 1e-6;
+
+        return Math.Abs(Math.Pow(_sideA, QuadraticFactor) + Math.Pow(_sideB, QuadraticFactor) -
+                        Math.Pow(_sideC, QuadraticFactor)) < eps ||
+               Math.Abs(Math.Pow(_sideB, QuadraticFactor) + Math.Pow(_sideC, QuadraticFactor) -
+                        Math.Pow(_sideA, QuadraticFactor)) < eps ||
+               Math.Abs(Math.Pow(_sideC, QuadraticFactor) + Math.Pow(_sideA, QuadraticFactor) -
+                        Math.Pow(_sideB, QuadraticFactor)) < eps;
+    }
+}
